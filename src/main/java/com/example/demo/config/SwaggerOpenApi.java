b29d6c6adcfd70a16b0e3c1a00ff03807e800d57
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2024-07-08 14:01:00 -0300

    feat(SwaggerOpenApi): add new configs to Jwt Authentication

--- src/main/java/com/example/demo/config/SwaggerOpenApi.java
+++ src/main/java/com/example/demo/config/SwaggerOpenApi.java
@@ -1,17 +1,45 @@
 package com.example.demo.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerOpenApi {
 
     @Bean
-    public GroupedOpenApi publicApi() {
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Users API")
+                        .version("1.0")
+                        .description("Users API")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                ).addSecurityItem(
+                        new SecurityRequirement()
+                                .addList("bearer-key", Arrays.asList("read", "write"))
+                );
+    }
+
+    @Bean
+    public GroupedOpenApi authorizationApi() {
         return GroupedOpenApi.builder()
-            .group("Public APIs")
-            .pathsToMatch("/public/**")
+            .group("Authorization APIs")
+            .pathsToMatch("/api/auth/**")
             .build();
     }
 
@@ -31,11 +59,4 @@ public class SwaggerOpenApi {
             .build();
     }
 
-    @Bean
-    public GroupedOpenApi allApi() {
-        return GroupedOpenApi.builder()
-            .group("All APIs")
-            .pathsToMatch("/**")
-            .build();
-    }
 }
