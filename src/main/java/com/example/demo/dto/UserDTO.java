a070259a5621a33c094a50785b5cfbc65175db8b
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2024-07-31 23:20:33 -0300

    feat(DTOs): create userDTOs and loginRegisterDTO

---
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String email;
+    private String fullName;
+    private List<String> roles;
+}
